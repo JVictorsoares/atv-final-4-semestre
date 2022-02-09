9f1abb1e88d3f1acb5feacc84d0a09c65eee50fc
joao victor pereira soares <joaosores4@gmail.com>
2022-02-09 20:44:20 -0300

    Initial commit

---
+++ atvquevale3/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace atvquevale3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(348, 192);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "grupo";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "selecionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(19, 75);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(96, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Entretenimento";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(19, 52);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(87, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Infraestrutura";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(19, 29);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(107, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Desenvolvimento";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(463, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(463, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "selecionar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(430, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(649, 445);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 653);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ atvquevale3/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atvquevale3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked == true){
+                comboBox1.Items.Add("1. Python");
+                comboBox1.Items.Add("2. Java");
+                comboBox1.Items.Add("3. C#");
+                comboBox1.Items.Add("4. JavaScript");
+                comboBox1.Items.Add("5. PHP");
+            }
+            if (radioButton2.Checked == true)
+            {
+                comboBox1.Items.Add("1. Azure");
+                comboBox1.Items.Add("2. AWS");
+                comboBox1.Items.Add("3. Cisco");
+                comboBox1.Items.Add("4. Dell");
+                comboBox1.Items.Add("5. Asus");
+            }
+            if (radioButton3.Checked == true)
+            {
+                comboBox1.Items.Add("2020 - Parasita");
+                comboBox1.Items.Add("2019 - Green Book: O Guia");
+                comboBox1.Items.Add("2018 - A Forma da Água");
+                comboBox1.Items.Add("2017 - Moonlight");
+                comboBox1.Items.Add("2016 - Spotlight: Segredos Revelados");
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "1. Python")
+            {
+                pictureBox1.Image = Properties.Resources.download;
+
+            }
+            if (comboBox1.Text == "2. Java")
+            {
+                pictureBox1.Image = Properties.Resources._2014_03_11_veja_como_resolver_problemas_com_o_java_no_pc;
+
+            }
+            if (comboBox1.Text == "3. C#")
+            {
+                pictureBox1.Image = Properties.Resources.main_qimg_f17348e944f5c865183e351175835273;
+
+            }
+            if (comboBox1.Text == "4. JavaScript")
+            {
+                pictureBox1.Image = Properties.Resources.javascript;
+
+            }
+            if (comboBox1.Text == "5. PHP")
+            {
+                pictureBox1.Image = Properties.Resources.PHP_logo_svg;
+
+            }
+            if (comboBox1.Text == "1. Azure")
+            {
+                pictureBox1.Image = Properties.Resources.Logo_Azure_684x226;
+
+            }
+            if (comboBox1.Text == "2. AWS")
+            {
+                pictureBox1.Image = Properties.Resources._1200px_Amazon_Web_Services_Logo_svg;
+
+            }
+            if (comboBox1.Text == "3. Cisco")
+            {
+                pictureBox1.Image = Properties.Resources.cisco;
+
+            }
+            if (comboBox1.Text == "4. Dell")
+            {
+                pictureBox1.Image = Properties.Resources.dell;
+
+            }
+            if (comboBox1.Text == "5. Asus")
+            {
+                pictureBox1.Image = Properties.Resources.asus;
+
+            }
+
+            if (comboBox1.Text== "2020 - Parasita")
+            {
+                pictureBox1.Image = Properties.Resources._1_Ij9SX7GcMTD6lzbI0jfLFw;
+
+            }
+            if (comboBox1.Text == "2019 - Green Book: O Guia")
+            {
+                pictureBox1.Image = Properties.Resources.green_book;
+
+            }
+            if (comboBox1.Text == "2018 - A Forma da Água")
+            {
+                pictureBox1.Image = Properties.Resources._3044833;
+
+            }
+            if (comboBox1.Text == "2017 - Moonlight")
+            {
+                pictureBox1.Image = Properties.Resources.Moonlight__filme_;
+
+            }
+            if (comboBox1.Text == "2016 - Spotlight: Segredos Revelados")
+            {
+                pictureBox1.Image = Properties.Resources._181510;
+
+            }
+        }
+    }
+}
